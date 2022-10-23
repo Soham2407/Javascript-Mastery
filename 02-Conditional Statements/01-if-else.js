b6026a160031a4212e03d72d53aa74b5d3fc68ea
Soham2407 <nesarikarsoham24@gmail.com>
2022-10-23 10:35:41 +0530

    initial commit

---
+++ 02-Conditional Statements/01-if-else.js
@@ -0,0 +1,15 @@
+const age = 16;
+
+if (age >= 18) {
+  console.log("You are able to drive");
+} else {
+  console.log("you are not able to drive");
+}
+
+// truthy and falsy values
+
+// falsy values
+// 0, false, "", null, undefined
+
+// truthy values
+// all are truthy values
---
+++ 02-Conditional Statements/02-ternary-operator.js
@@ -0,0 +1,13 @@
+// ternary operator
+const age = 31;
+
+const result = age >= 18 ? "You are adult" : "You are child";
+
+console.log(result);
+
+// == vs ===
+const num1 = "6";
+const num2 = 6;
+
+console.log(num1 == num2);
+console.log(num1 === num2);
